21defc04a4934ca4c1034492489b86cf38b274b1
Vikram Bhavsar <vikram.bhavsar@somaiya.edu>
2022-04-04 22:33:36 +0530

    Added CRUD for projects

--- apicalls/urls.py
+++ apicalls/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('all-projects',views.ProjectList.as_view(),name='all-projects'),
+    path('projects',views.ProjectList.as_view(),name='projects'),
+    path('project/<pk>',views.ProjectList.as_view(),name='project'),
     path('', views.index, name='index'),
 ]
--- apicalls/views.py
+++ apicalls/views.py
@@ -1,9 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from rest_framework import viewsets
-from rest_framework import permissions
+from rest_framework import viewsets, permissions, mixins, generics
 from .serializers import ProjectSerializer, MediaContentSerializer, SectionSerializer,ProjectMainSerializer
-from rest_framework import generics
 from .models import Project
 
 # Create your views here.
@@ -11,9 +9,21 @@ def index(request):
     return HttpResponse("hello World")
 
 # This view will show all the projects
-class ProjectList(generics.ListAPIView):
+class ProjectList(generics.ListAPIView, mixins.CreateModelMixin,mixins.DestroyModelMixin,mixins.UpdateModelMixin):
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
+
+    # for Creating a new project
+    def post(self, request, *args, **kwargs):
+        return self.create(request,*args,**kwargs)
+
+    # for updating an existing project
+    def put(self, request, *args, **kwargs):
+        return self.update(request,*args,**kwargs)
+
+    # for deleting an existing project
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request,*args,**kwargs)
 
 
 # This view will show all the data combined, includes projects, sections and media content.s
