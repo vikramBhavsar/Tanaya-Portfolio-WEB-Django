7adf1090b66e78b3d6159e73935caa38cbda76ac
Vikram Bhavsar <vikram.bhavsar@somaiya.edu>
2022-04-04 21:55:58 +0530

    Initial List Serializers are setuo

--- apicalls/urls.py
+++ apicalls/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('all-projects',views.ProjectList.as_view(),name='all-projects'),
     path('', views.index, name='index'),
 ]
--- apicalls/views.py
+++ apicalls/views.py
@@ -1,6 +1,22 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework import permissions
+from .serializers import ProjectSerializer, MediaContentSerializer, SectionSerializer,ProjectMainSerializer
+from rest_framework import generics
+from .models import Project
 
 # Create your views here.
 def index(request):
     return HttpResponse("hello World")
+
+# This view will show all the projects
+class ProjectList(generics.ListAPIView):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+
+
+# This view will show all the data combined, includes projects, sections and media content.s
+class AllDataList(generics.ListAPIView):
+    queryset = Project.objects.all()
+    serializer_class = ProjectMainSerializer
