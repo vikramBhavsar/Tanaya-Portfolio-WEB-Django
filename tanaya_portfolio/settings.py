a6d80c8453c14e4bb607b4ce3acf534ab30c3243
rahulsanghvi18 <rahulsanghvi18@gmail.com>
2022-05-12 20:41:05 +0530

    changes for CORS - 1

--- tanaya_portfolio/settings.py
+++ tanaya_portfolio/settings.py
@@ -46,12 +46,13 @@ INSTALLED_APPS = [
     'rest_framework',
     'apicalls',
     'rest_framework_simplejwt',
-    'corsheaders',
     'django_cleanup.apps.CleanupConfig',
     'storages'
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -59,12 +60,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware'
 ]
 
 
 # CSRF 
-CSRF_TRUSTED_ORIGINS = config("CORS_ALLOWED_ORIGINS", cast=lambda v: [s.strip() for s in v.split(',')])
+CSRF_TRUSTED_ORIGINS = config("CSRF_TRUSTED_ORIGINS", cast=lambda v: [s.strip() for s in v.split(',')])
 
 # CORS policy - allowing only certain links
 CORS_ORIGIN_ALLOW_ALL = False
